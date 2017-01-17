5565952442ca0ce93895241f810da6b7cdf7a7c8
Maksim <maximnorby@gmail.com>
2017-01-17 23:16:46 +0200

    footer section is ready template code is finished further  I'll do adaptive code for tablets and mobile phones

--- js/common.js
+++ js/common.js
@@ -10,3 +10,43 @@ $(document).ready(function() {
 	});
 	
 });
+
+$(document).ready(function() {
+
+    $(".top_menu_button").click(function() {
+        $(".top_menu ul").slideToggle();
+    });
+
+    $(".top_menu").find("a").click(function() {
+
+        if ($( window ).width() < 1024) {
+            $(".top_menu ul").slideToggle();
+        }
+    });
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    $("a.scroll").click(function() {
+        $.scrollTo($(".div"), 800, {
+            offset: -90
+        });
+    });
+
+    $("#top_menu").click(function () {
+        $("body, html").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+});
